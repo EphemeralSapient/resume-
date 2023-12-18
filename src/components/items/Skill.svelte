b30449c0bb3bc18767288dca861267cc6e324d82
EphemeralSapient <nowayofway@gmail.com>
2023-12-19 00:00:30 +0530

    Commit

---
+++ src/components/items/Skill.svelte
@@ -0,0 +1,18 @@
+<script>
+	export let skill;
+</script>
+
+<style>
+	.skill {
+		border: 2px solid #0000006c;
+		width: fit-content;
+		padding: 8px 12px; /* Increased padding for a more spacious look */
+		border-radius: 5px;
+		margin: 6px;
+		font-size: 1em; /* Adjusted font size for better readability */
+		font-weight: 400; /* Increased font weight for emphasis */
+
+    }
+</style>
+
+<div class="skill">{skill}</div>
---
+++ src/components/structural/Header.svelte
@@ -0,0 +1,46 @@
+<script>
+	export const name = 'Ilavarasan';
+	const summaryLines = [
+		'As a dedicated Computer Science Engineering student entering my third year, I am fervently curious about emerging technologies and frameworks. My passion for technology extends beyond the classroom, driving me to explore the depths of Linux and engage with open-source software.',
+	];
+</script>
+
+<style>
+	#header {
+		display: flex;
+		flex-direction: column;
+		align-items: center;
+		justify-content: space-between;
+		text-align: center;
+		margin-bottom: var(--vertical-space-between-sections);
+	}
+
+	#my-name {
+		text-transform: uppercase;
+		color: var(--accent-color);
+		font-family: var(--accent-font);
+		font-size: 3em;
+		margin: 0;
+	}
+
+	.summary {
+		width: 78%;
+		margin-top: 5px;
+		font-size: 1.05em;
+		font-weight: 300;
+		font-family: var(--accent-font);
+		line-height: 1.5em;
+		color: #000;
+	}
+</style>
+
+<div id="header">
+	<h1 id="my-name">{name}</h1>
+	<div class="summary">
+		{#each summaryLines as line, i}
+			<div class="summary-line-{i}">
+				{@html line}
+			</div>
+		{/each}
+	</div>
+</div>
